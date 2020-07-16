ba5e7ff5bda766a6f7e713e2e2bfa8aa951351ce
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-07-16 19:53:03 +0900

    여기까지가 class 공부하기 전

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+//구조분해할당 사용 !! 
+function Food({ name, picture, rating }) {
+  return(
+    <div>
+      <h2>I like {name}</h2>;
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+    //alt는 시각 장애인을 위한 것임.
+  );
+
+}
+//리액트에서 객체는 중괄호 양쪽을 공백으로 채욱, JSX의 자바스크립트는 중괄호 양쪽을 공백으로 채우지 않는 방법으로 구분한다.
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://cdn.crowdpic.net/detail-thumb/thumb_d_AAA4C5E8C1E29791413BFBFF5419BA73.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg',
+    rating:  3,
+  },
+  {
+    id: 4,
+    name: 'Kimbap',
+    image: 'https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG',
+    rating: 2,
+  },
+];
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+    <div>
+      {foodILike.map(dish => ( <Food key={dish.id} name={dish.name} picture= {dish.image} rating={dish.rating} />))}
     </div>
+    //key props는 리액트 내부에서 사용되는 특수한 props라서 Food 컴포넌트에 직접 전달되지 않는다. 
+    
   );
+      // Food컴포넌트에 사용한 props의 이름은 fav이고, fav에 "Kimchi"라는 값을 담아 Food 컴포넌트에 보냈다. 
+      // props에 있는 데이터는 문자열인 경우를 제외하면 모두 중괄호 {}로
+  
+  
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  Picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
+  // <App />은 App 컴포넌트를 임포트 하여 사용한 것임 
+  // App 컴포넌트는 아이디가 root인 엘리먼트에 그려질 것이다. 
+  // 리액트는 컴포넌트와 함께 동작하고 리액트 앱은 모두 컴포넌트로 구성된다. 
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
