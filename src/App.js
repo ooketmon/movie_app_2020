cec33884605566e9b515e065c8e305d69569a026
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-07-17 16:40:59 +0900

    API사용, 화면에 나타내기, 기초 스타일링

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-
-    count: 0, 
-
+    isLoading: true, 
+    movies: [],
   };
-  //state를 사용하려면 클래스형 컴포넌트 안에서 소문자롤 이용해서 state라고 적자...
-  //stae는 동적 데이터 활용시 꼭 필요 !!!!!
-
-  add = () => {
-    //this.state.count = 1; state 직접 변경하지 마라... 왜? render 다시 못불러오거든! 그럼 해결책은? -> setState 함수 ! 
-    //this.setState({ count: this.state.count + 1 }); 좋은 방법은 아님,, 왜? 성능 문제가 있을 수 있다고 함 ,,!
-    this.setState(currnet => ({count: currnet.count + 1, }));
-  };
-
-  minus = () => {
-    //this.state.count = -1; state 직접 변경하지 마라...
-    //this.setState({ count: this.state. count - 1 });
-    this.setState(currnet => ({count: currnet.count - 1, }));
+  getMovies = async () => {
+    const {
+      data:{
+        data:{movies},
+      },
+    
+    }= await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   }
-
-  //리액트 반응속도 짱빠르다. 새로고침도 안일어남 bb 
-
   componentDidMount() {
-    console.log('component rendered');
+   this.getMovies();
   }
 
-  componentDidUpdate() { //화면이 업데이트 되는 경우에 실행됨
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() { //컴포넌트가 화면에서 떠날 때 실행 됨
-    console.log('Goodbye, cruel world');
-  }
+  //async는 자바스크립트에게 getMovies가 비동기라고 말해주는 것. 그니까 기다리셈 ! 이라고 말해주는 것이다. 
+  //await은 자바스크립트에게 getMovies 함수 내부의 axious.get가 실행 완료되기 까지 기다렸다가 끝나면 계속 해! 라고 말해주는 것
+  //==> API에서 받아오는 axious.get을 실행하려면 시간이 필요하고, 그걸 자바스크립트한테 알려줘야 하니까 async랑 await을 넣은 것이다. 
 
   render() {
-    console.log("I'm rendering");
-    return (
-    <div>
-    <h1>The number is : {this.state.count}</h1>
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading, movies } = this.state;
+    return (<section class = "container">
+      {isLoading ? (
+        <div class = "loader">
+          <span class = "loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div class = "movies">
+            {movies.map((movie) => (
+              <Movie
+                key = {movie.id}
+                id = {movie.id}
+                year ={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}      
+              />
+        ))}
+          </div>
+        )}
+      </section>
     );
   }
-//클래스형 컴포넌트는 render()함수가 JSX를 반환한다. -> render()함수는 우리가 직접 실행하지 않아도 됨.
 }
-//클래스형 컴포넌트가 되려면 'App클래스가 리액트가 제공하는 Component 클래스를 반드시 상속 :: 클래스에 다른 클래스의 기능을 추가할 수 잇게 해준다 ::받아야 한다 !!! '
 
 export default App;
