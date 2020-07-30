2f2a5907de533d5f8610ee8e3430d7d4be8149bf
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-07-31 02:12:16 +0900

    Updates

---
+++ precache-manifest.8cbfea972549c38b1874d9cd1216adfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebe44b955c3d92c3ff571c30c969510",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f515563c16505335aa22",
+    "url": "/movie_app_2020/static/css/main.8df12ce0.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f515563c16505335aa22",
+    "url": "/movie_app_2020/static/js/main.8c4fcf4a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
